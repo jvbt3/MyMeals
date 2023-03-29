0aab867124bcfd2f25615cffc61fe34920cf3b81
jvbt3 <joaovitormartins077@gmail.com>
2023-03-29 10:38:57 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(
+            primarySwatch: Colors.pink,
+          ).copyWith(
+            secondary: Colors.amber,
+          ),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('myMeals'),
+      ),
+      body: const Center(
+        child: Text('Navegar é preciso!!'),
+      ),
+    );
+  }
+}
